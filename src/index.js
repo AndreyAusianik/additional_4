b7e897556f596ee539531c8ca4bdadc7fa9bcb99
Andrei Ausianik <andrei.ausianik@iii.com>
2017-10-23 14:57:11 +0300

    product solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
-module.exports = function multiply(first, second) {
-  // your solution
+module.exports = (first, second) => new BigInteger(first).mull(new BigInteger(second));
+
+const integerLength = Number.MAX_SAFE_INTEGER.toString().length / 2 - 1 | 0;
+const numberSplitRegExp = new RegExp(`\\d{1,${integerLength}}(?=(\\d{${integerLength}})*$)`, 'g');
+
+class BigInteger {
+    constructor(string) {
+        if(string.length > integerLength) {
+            this.numberArray = string.match(numberSplitRegExp).map(parseFloat).reverse();
+        } else {
+            this.numberArray = [+string];
+        }
+    }
+
+    mull(second) {
+        const result = [];
+        this.numberArray.forEach((numLeft, i) => {
+           second.numberArray.forEach((numRight, j) => {
+               result[i + j] = (result[i + j] || 0) + numLeft * numRight;
+           });
+        });
+
+        result.forEach((element, idx, arr) => {
+            const numberStr = element.toString();
+            if(numberStr.length > integerLength) {
+                const nextRank = +numberStr.slice(0, -integerLength);
+                const currentRank = +numberStr.slice(-integerLength);
+                arr[idx + 1] = (arr[idx + 1] || 0) + nextRank;
+                arr[idx] = currentRank;
+            }
+        });
+
+        return result.reverse()
+            .map(num => '0'.repeat(integerLength - num.toString().length) + num)
+            .join``.replace(/^0+(?!$)/, '');
+    }
 }
